a93df52bd079bcc1879e964a89fb9b11f84dbf96
ilknur <ilknur_daglar@yahoo.com>
2023-01-23 22:05:57 +0100

    added configuration.properties added CukesRunner, Driver, HomePage, HomeStepDef,

---
+++ src/test/java/com/ismakinesi/runners/CukesRunner.java
@@ -0,0 +1,22 @@
+package com.ismakinesi.runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+            plugin = {"json:target/cucumber.json",
+                    "html:target/default-html-reports"},
+            features = "src/test/resources/features",
+            glue = "com/ismakinesi/stepDefinitions",
+            dryRun = false,
+            tags = "@login"
+    )
+
+ public class CukesRunner{
+
+    }
+
+
---
+++ src/test/java/com/ismakinesi/stepDefinitions/HomeStepDef.java
@@ -0,0 +1,4 @@
+package com.ismakinesi.stepDefinitions;
+
+public class HomeStepDef {
+}
