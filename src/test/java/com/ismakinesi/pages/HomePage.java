09b8a652307aa57a5e60f6ca56a19128394523ef
90551 <akarsu64engin@gmail.com>
2023-01-27 22:44:32 +0300

    Home_Page Locate updated

--- src/test/java/com/ismakinesi/pages/HomePage.java
+++ src/test/java/com/ismakinesi/pages/HomePage.java
@@ -9,7 +9,8 @@ public class HomePage {
     public HomePage(){
         PageFactory.initElements(Driver.get(),this);
     }
-//***Locate başlama ****Engin Akgül tarafından hazırlanan "Tüm Filtreler ve Ana Sayfa Locate leridir.
+//***Locate başlama ****Engin Akgül tarafından hazırlanan "Tüm Filtreler ve Ana Sayfa" Locate leridir.
+
     //****Tüm Filitreler***
 
     @FindBy(xpath = "//div[@class='filter']")
@@ -18,49 +19,8 @@ public class HomePage {
     @FindBy(xpath = "//h3[.='Kategori']")
     public WebElement Category;
 
-
     @FindBy(xpath = "//*[@id='content']/div/div[1]/input")
     public WebElement Search_input;
-
-    @FindBy(xpath = "//h3[.='Marka'']")
-    public WebElement brand;
-
-    @FindBy(xpath = "//h3[.='Sınırlı Servis Garantisi']")
-    public WebElement Limited_Service;
-
-    @FindBy(xpath = "//h3[.='Üretim Yılı']")
-    public WebElement year_Of_Production ;
-
-    @FindBy(xpath = "//h3[.='Çalışma Saati']")
-    public WebElement study_time;
-
-    @FindBy(xpath = "//h3[.='Fiyat']")
-    public WebElement price;
-
-    @FindBy(xpath = "//a[contains(text(),'Son ilanlar')]")
-    public WebElement end_listings;
-
-
-    @FindBy(xpath = "//a[contains(text(),'Son görüntülenenler')]")
-    public WebElement end_viewed;
-
-    @FindBy(xpath = "//a[contains(text(),'Paletli Ekskavatör')]")
-    public WebElement Crawler_Excavator;
-
-
-    @FindBy(xpath = "//a[contains(text(),'Lastik Tekerlekli Yükleyici')]")
-    public WebElement Wheel_Loader;
-
-    @FindBy(xpath = "//a[contains(text(),'Akülü Forklift')]")
-    public WebElement Battery_Forklift;
-
-    @FindBy(xpath = "//a[contains(text(),'Kazıcı Yükleyici')]")
-    public WebElement Backhoe_Loader;
-
-    @FindBy(xpath = "//a[contains(text(),'Mini Ekskavatör (<12t)')]")
-    public WebElement Mini_Excavator;
-
-
 
     @FindBy(xpath = "//*[text()='Dizel Forklift']")
     public WebElement Radio_Dizel_Forklift;
@@ -88,6 +48,48 @@ public class HomePage {
 
     @FindBy(xpath = "//input[@fdprocessedid='m0qkvb']")
     public WebElement max_price;
+
+
+    @FindBy(xpath = "//h3[.='Marka'']")
+    public WebElement brand;
+
+    @FindBy(xpath = "//h3[.='Sınırlı Servis Garantisi']")
+    public WebElement Limited_Service;
+
+    @FindBy(xpath = "//h3[.='Üretim Yılı']")
+    public WebElement year_Of_Production ;
+
+    @FindBy(xpath = "//h3[.='Çalışma Saati']")
+    public WebElement study_time;
+
+    @FindBy(xpath = "//h3[.='Fiyat']")
+    public WebElement price;
+
+    //***Ana Sayfa Locate***///
+    @FindBy(xpath = "//a[contains(text(),'Son ilanlar')]")
+    public WebElement end_listings;
+
+
+    @FindBy(xpath = "//a[contains(text(),'Son görüntülenenler')]")
+    public WebElement end_viewed;
+
+    @FindBy(xpath = "//a[contains(text(),'Paletli Ekskavatör')]")
+    public WebElement Crawler_Excavator;
+
+
+    @FindBy(xpath = "//a[contains(text(),'Lastik Tekerlekli Yükleyici')]")
+    public WebElement Wheel_Loader;
+
+    @FindBy(xpath = "//a[contains(text(),'Akülü Forklift')]")
+    public WebElement Battery_Forklift;
+
+    @FindBy(xpath = "//a[contains(text(),'Kazıcı Yükleyici')]")
+    public WebElement Backhoe_Loader;
+
+    @FindBy(xpath = "//a[contains(text(),'Mini Ekskavatör (<12t)')]")
+    public WebElement Mini_Excavator;
+
+
 
 
 //***Locate bitiş ****Engin Akgül tarafından hazırlanan "Tüm Filtreler ve Ana Sayfa Locate leridir.
